6c3bd34c5f192b5324db89de222a3ed68d1ce7e6
Christoffer Fridlund <christoffer.fridlund@hotmail.com>
2015-09-19 16:50:17 +0300

    Ship controls and textures.

---
+++ UnityProject/Assets/Scripts/BillboardShip.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BillboardShip : MonoBehaviour {
+
+    public Transform targetTransform;
+	
+	// Update is called once per frame
+	void Update () {
+        transform.LookAt(targetTransform.position, Vector3.up);
+    }
+}
--- UnityProject/Assets/Scripts/PlayerController.cs
+++ UnityProject/Assets/Scripts/PlayerController.cs
@@ -1,18 +1,60 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour
+{
 
     // link to the current orbit
     public Orbit orbit;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    public float angle;
+    public float radius;
+
+    public float rotationImpulse = 0.01f;
+    public float damping = 0.95f;
+    public float acceleration = 0.1f;
+
+    private float rotationSpeed;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Always apply the damping
+        rotationSpeed *= damping;
+
+        // Add impulse to the rotationSpeed
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            rotationSpeed = rotationImpulse;
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            rotationSpeed = -rotationImpulse;
+        }
+
+        // Continue the movement by applying a small acceleration
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            rotationSpeed += acceleration * Time.deltaTime;
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rotationSpeed -= acceleration * Time.deltaTime;
+        }
+        angle += rotationSpeed;
+
+        if (orbit != null)
+        {
+            radius = orbit.radius;
+        }
+
+        transform.localPosition = new Vector3(radius * Mathf.Cos(angle), transform.localPosition.y, radius * Mathf.Sin(angle));
+    }
+
+
 }
